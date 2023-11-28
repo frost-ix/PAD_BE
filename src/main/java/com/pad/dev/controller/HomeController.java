ca2b8ede3cf563c7dd2b0ccb0396b58649cd45cc
YongQ <cyk728@naver.com>
2023-11-28 17:36:53 +0900

    configuration, mapper

--- src/main/java/com/pad/dev/controller/HomeController.java
+++ src/main/java/com/pad/dev/controller/HomeController.java
@@ -1,8 +1,13 @@
 package com.pad.dev.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    @GetMapping("test")
+    public String test() {
+        return "test";
+    }
     
 }
