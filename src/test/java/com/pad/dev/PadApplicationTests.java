af9c2276854e6b6b8fc1216f51d0d2ad2045b170
frost-secu <shw9810@icloud.com>
2023-11-28 16:14:27 +0900

    first commit

---
+++ src/test/java/com/pad/dev/PadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pad.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
