9bea0f8ea322c494fd902e814467200add928d85
YongQ <cyk728@naver.com>
2023-12-06 17:01:20 +0900

    깔끔하게 정리

--- src/main/java/com/pad/dev/service/HomeService.java
+++
@@ -1,9 +0,0 @@
-package com.pad.dev.service;
-
-import java.util.List;
-
-import com.pad.dev.vo.boardVO.BoardVO;
-
-public interface HomeService {
-    List<BoardVO> getBoardList();
-}
--- src/main/java/com/pad/dev/serviceImpl/BoardServiceImple.java
+++ src/main/java/com/pad/dev/serviceImpl/BoardServiceImple.java
@@ -2,13 +2,11 @@ package com.pad.dev.serviceImpl;
 
 import java.util.List;
 
-import org.apache.ibatis.javassist.compiler.ast.Member;
 import org.springframework.stereotype.Service;
 
 import com.pad.dev.dao.BoardDAO;
 import com.pad.dev.service.BoardService;
 import com.pad.dev.vo.boardVO.BoardVO;
-import com.pad.dev.vo.memberVO.MemberVO;
 
 import lombok.RequiredArgsConstructor;
 
